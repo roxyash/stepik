33a9710c2b092b47915871f1ef87b3c8f30eaee7
roxyash <roxyash@mail.ru>
2022-11-16 01:17:59 +0600

    resolve 2.7.2 programmingongo course

--- ProgrammingOnGo/2.7.2/main.go
+++ ProgrammingOnGo/2.7.2/main.go
@@ -1,6 +1,17 @@
 // На вход подаются a и b - катеты прямоугольного треугольника. Нужно найти длину гипотенузы
 package main
 
-func main(){
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var a, b float64
+	_, err := fmt.Scan(&a, &b)
+	if err != nil {
+		fmt.Printf("Scan err: %v", err.Error())
+	}
+
+	fmt.Print(math.Sqrt(a*a+b*b))
 }
